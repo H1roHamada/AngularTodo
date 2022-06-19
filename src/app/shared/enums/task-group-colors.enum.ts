448a803f1c75eac86cb4c387f5fa5cc2252ceeeb
Hiro Hamada <you@example.com>
2022-06-19 18:07:11 +1000

    upd: добавил цвета для задач

---
+++ src/app/shared/enums/task-group-colors.enum.ts
@@ -0,0 +1,10 @@
+export enum TaskGroupColorsEnum {
+  grey = '#c9d1d3',
+  green = '#42b883',
+  blue = '#64c4ed',
+  pink = '#ffbbcc',
+  lime = '#b6e6bd',
+  purple = '#c355f5',
+  black = '#09011a',
+  red = '#ff6464',
+}
